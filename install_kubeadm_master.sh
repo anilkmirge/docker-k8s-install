490d1f29531236fb66d854fb16da5b25bc19a48f
Anil Mirge <manilche@gmail.com>
2020-05-17 22:39:01 -0400

    Cleaning up the scripts

--- install_kubeadm_master.sh
+++ install_kubeadm_master.sh
@@ -28,29 +28,16 @@ echo 'Disabling swap in order for the kubelet to work properly.'
 swapoff -a
 kubeadm init --kubernetes-version $(kubeadm version -o short) --pod-network-cidr=192.168.0.0/16 | tee /tmp/kubeinit.log
 
-echo 'To start using your cluster, running some more commands as a regular user'
 
-# To make kubectl work for your root user, you can run
-
-export KUBECONFIG=/etc/kubernetes/admin.conf
-
-# To make kubectl work for your non-root user, run these commands, which are also part of the kubeadm init output:
+echo 'To make kubectl work for your non-root user, running these commands, which are also part of the kubeadm init output:
 
 # mkdir -p $HOME/.kube
 # sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-# sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config'
 
-#Then you can join any number of worker nodes by running the following on each as root:
-
-# sudo kubeadm join 10.0.1.11:6443 --token 264t3t.rqy1qaf0dmfpeg5f 
-# --discovery-token-ca-cert-hash sha256:68c340191ad5304bca36d3dd5570feb5bc2fbb29c8c1e389f1caa621b9e9c917
-
-# Note: By default, tokens expire after 24 hours. If you are joining a node to the cluster after the current token has expired, you can create a new token by running the following command on the control-plane node:
-
-# sudo kubeadm token create --print-join-command
-# The output is similar to this:
-
-# 5didvk.d09sbcov8ph2amjw
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo 'Deploying an Overlay POD Network'
 echo 'Calico will automatically detect which IP address range to use for pod IPs based on the value provided via the --pod-network-cidr flag or via kubeadm’s configuration.'
@@ -59,6 +46,4 @@ kubectl apply -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
 
 echo 'Once a Pod network has been installed, you can confirm that it is working by checking that the CoreDNS Pod is Running in the output of kubectl get pods --all-namespaces.'
 
-kubectl get pods --all-namespaces.
-
-exit
+kubectl get pods --all-namespaces
